aa8a4531f457c98a51966d2ac8bf8418da95a7a1
Govind Kumar <gk4051668@gmail.com>
2023-03-26 00:50:11 +0530

    added popup

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -5,15 +5,26 @@ import { response } from "../pages/fetch";
 import { logout } from "../redux/movies.slice";
 import { MovieCard } from "./MovieCard";
 
+let popup;
 export const MoviesList = () => {
   const [movies, setMovies] = useState(response.result);
   const dispatch = useDispatch();
   const { isLoading, isError, username } = useSelector((state) => state.movies);
 
+  useEffect(() => {
+    popup = document.querySelector(".popup");
+  }, []);
+
   return (
     <MoviesSection>
       <div className="section_movies_header">
-        All Movies{" "}
+        <h3> All Movies</h3>
+        <button
+          className="btn info"
+          onClick={() => popup.classList.toggle("showpopup")}
+        >
+          Company Info
+        </button>
         <button className="btn login" onClick={() => dispatch(logout())}>
           logout
         </button>
@@ -27,6 +38,38 @@ export const MoviesList = () => {
           ))}
         </div>
       )}
+      <div className="popup">
+        <div>
+          <span>
+            <b>Company: </b>
+          </span>
+          Geeksynergy Technologies Pvt Ltd
+        </div>
+        <div>
+          <span>
+            <b>Address: </b>
+          </span>
+          Sanjayanagar, Bengaluru-56
+        </div>
+        <div>
+          <span>
+            <b>Email: </b>
+          </span>
+          XXXXXX@gmail.com
+        </div>
+        <div>
+          <span>
+            <b>Phone: </b>
+          </span>
+          XXXXXXXXX09
+        </div>
+        <p
+          className="btn info"
+          onClick={() => popup.classList.toggle("showpopup")}
+        >
+          close
+        </p>
+      </div>
     </MoviesSection>
   );
 };
@@ -42,7 +85,17 @@ const MoviesSection = styled.section`
     color: #0081c9;
     font-weight: bold;
     font-size: 30px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    gap: 10px;
 
+    .info {
+      background: var(--second-color);
+      border-radius: 5px;
+      font-size: large;
+      text-transform: uppercase;
+    }
     /* border: 1px solid black; */
 
     position: relative;
@@ -52,9 +105,9 @@ const MoviesSection = styled.section`
       font-size: large;
       text-transform: uppercase;
 
-      position: absolute;
+      /* position: absolute;
       top: 0;
-      right: 0;
+      right: 0; */
     }
   }
 
@@ -64,7 +117,20 @@ const MoviesSection = styled.section`
     gap: 1rem;
     padding: 1rem;
   }
-
+  .popup {
+    position: fixed;
+    top: -30%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: var(--first-color);
+    padding: 1rem;
+    color: white;
+    font-size: 20px;
+    transition: 0.3s all ease-in-out;
+  }
+  .showpopup {
+    top: 30%;
+  }
   @media (max-width: 760px) {
     .movies {
       grid-template-columns: repeat(2, 1fr);
